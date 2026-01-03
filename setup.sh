55effd26bb3a474840ff0f975020b110ea01b635
kev1nandreas <kevin.andreascn@gmail.com>
2026-01-03 12:36:19 +0700

    fix: Enhance setup script output with emojis for better visibility and correct directory references

--- setup.sh
+++ setup.sh
@@ -5,26 +5,26 @@ BASE_WORKING_DIR=$1
 # Install Docker and Docker Compose
 sudo apt update
 sudo apt install -y docker.io docker-compose
-echo "Docker and Docker Compose installed."
+echo "🟢 Docker and Docker Compose installed."
 
 # Enable and start Docker service
 sudo systemctl enable docker
 sudo systemctl start docker
-echo "Docker service started."
+echo "🟢 Docker service started."
 
 # Make Directory for Monitoring
 mkdir -p $BASE_WORKING_DIR/monitoring
 cd $BASE_WORKING_DIR/monitoring
-echo "Created monitoring directory at $BASE_WORKING_DIR/monitoring."
+echo "🟢 Created monitoring directory at $BASE_WORKING_DIR/monitoring."
 
 # Create Prometheus Data Directory
 mkdir -p $BASE_WORKING_DIR/monitoring/prometheus
-echo "Created Prometheus data directory at $BASE_WORKING_DIR/monitoring/prometheus."
+echo "🟢 Created Prometheus data directory at $BASE_WORKING_DIR/monitoring/prometheus."
 
 # Copy Prometheus Configuration File
-cp $BASE_WORKING_DIR/grafana-setup-vps-docker/prometheus.yml $BASE_WORKING_DIR/monitoring/prometheus/prometheus.yml
-echo "Copied Prometheus configuration file to $BASE_WORKING_DIR/monitoring/prometheus/prometheus.yml."
+cp $BASE_WORKING_DIR/grafana-setup-vps-dockerize/prometheus.yml $BASE_WORKING_DIR/monitoring/prometheus/prometheus.yml
+echo "🟢 Copied Prometheus configuration file to $BASE_WORKING_DIR/monitoring/prometheus/prometheus.yml."
 
 # Run Docker Compose to Set Up Monitoring Stack
-docker-compose -f $BASE_WORKING_DIR/grafana-setup-vps-docker/docker-compose.yml up -d
-echo "Monitoring stack is set up and running in detached mode".
+docker-compose -f $BASE_WORKING_DIR/grafana-setup-vps-dockerize/docker-compose.yml up -d
+echo "🟢 Monitoring stack is set up and running in detached mode."
